f00402bb8fde69dc70b1a65f6211ebd605bc5dae
harvin20 <chantutharvinalberto14@gmail.com>
2026-09-03 17:12:14 -0600

    first commit

---
+++ src/main/java/com/tecnm/merida/market_backend_26_3/MarketBackend263Application.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.market_backend_26_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackend263Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackend263Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnm/merida/market_backend_26_3/MarketBackend263ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnm.merida.market_backend_26_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackend263ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
